a2663f0df3574bb6a892ee2895e67f68bd9b235e
shixuewen <friendlysxw@163.com>
2021-04-29 18:13:16 +0800

    loader配置：加载图片

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import _ from 'lodash';
 import './style.css';
+import NotIeImgSrc from './not_ie.png'
 function component(){
     const element=document.createElement('div');
     // lodash 在当前 script 中使用 import 引入
     element.innerHTML = _.join(['Hello','webpack'],'');
     element.classList.add('hello')
+
+    // 将图像添加到我们已经存在的div中
+    const myImage = new Image();
+    myImage.src=NotIeImgSrc;
+    element.appendChild(myImage);
+    
     return element;
 }
 document.body.appendChild(component());
