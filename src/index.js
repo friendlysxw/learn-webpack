0122e615e4fc8398c240e9a5c66f196410178e64
shixuewen <friendlysxw@163.com>
2021-04-29 18:01:58 +0800

    loader配置：让js可以import css文件

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-import _ from 'lodash'
+import _ from 'lodash';
+import './style.css';
 function component(){
     const element=document.createElement('div');
     // lodash 在当前 script 中使用 import 引入
     element.innerHTML = _.join(['Hello','webpack'],'');
+    element.classList.add('hello')
     return element;
 }
 document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,14 @@ module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname,'dist'),
-        filename: 'main.js',
+        filename: 'bundle.js',
+    },
+    module:{
+        rules: [
+            {
+                test:/\.css$/i,
+                use:['style-loader','css-loader'],
+            }
+        ]
     }
 }
