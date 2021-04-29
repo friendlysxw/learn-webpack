3f3272bdd862a4aefe972e351e6a6a21d8e5b2b7
shixuewen <friendlysxw@163.com>
2021-04-29 18:40:58 +0800

    loader配置：加载字体

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ import NotIeImgSrc from './not_ie.png'
 function component(){
     const element=document.createElement('div');
     // lodash 在当前 script 中使用 import 引入
-    element.innerHTML = _.join(['Hello','webpack'],'');
+    element.innerHTML = _.join(['Hello','webpack',' 小兔兔那么可爱 '],'');
     element.classList.add('hello')
 
     // 将图像添加到我们已经存在的div中
     const myImage = new Image();
     myImage.src=NotIeImgSrc;
     element.appendChild(myImage);
-    
+
     return element;
 }
 document.body.appendChild(component());
