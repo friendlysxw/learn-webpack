3bafa06fcee60c0ea9ff6fe283a8d710702b904c
shixuewen <friendlysxw@163.com>
2021-04-30 17:12:23 +0800

    plugin配置：使用HtmlWebpackPlugin插件自动生成index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
     entry: {
         index: './src/index.js',
         print: './src/print.js'
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: '管理输出',
+        })
+    ],
     output: {
         path: path.resolve(__dirname,'dist'),
         filename: '[name].bundle.js',
